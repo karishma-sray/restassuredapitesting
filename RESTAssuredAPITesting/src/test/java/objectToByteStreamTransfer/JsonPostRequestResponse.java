fa4616fdf55e68d261412078990767ad944687f8
karishma.sray <ksamantaray2109@gmail.com>
2025-02-14 10:21:05 +0530

    deserialization2

---
+++ RESTAssuredAPITesting/src/test/java/objectToByteStreamTransfer/JsonPostRequestResponse.java
@@ -0,0 +1,15 @@
+package objectToByteStreamTransfer;
+
+public class JsonPostRequestResponse {
+	/*{
+    "name": "morpheus",
+    "job": "leader",
+    "id": "264",
+    "createdAt": "2025-02-14T04:28:42.343Z"
+}*/
+	//create variables for the above json data
+	public String name;
+	public String job;
+	public String id;
+	public String createdAt;
+}
